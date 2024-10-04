372ef7df8c7139c6fdc1249ce0538d6d2f5b96fc
Hirokixd <86230878+Wrdn28@users.noreply.github.com>
2024-10-04 12:39:47 +0700

    vendor: Bump to Andromeda version

--- config/version.mk
+++ config/version.mk
@@ -2,7 +2,7 @@ PRODUCT_VERSION_MAJOR = 14
 PRODUCT_VERSION_MINOR = 0
 
 # Increase CR Version with each major release.
-ORION_VERSION := Centauri
+ORION_VERSION := Andromeda
 
 # OOS maintainer flags
 ORION_MAINTAINER ?= Unknown
